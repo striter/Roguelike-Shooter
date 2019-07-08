7841308e15d4313db6dd379f32c5e016928c8739
striter <letsbattle@foxmail.com>
2019-07-08 09:37:15 +0800

    Meteor Param Update

--- Assets/Script/InGame/SFXProjectileCastMeteor.cs
+++ Assets/Script/InGame/SFXProjectileCastMeteor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class SFXProjectileCastMeteor : SFXProjectileCastTrigger
 {
     public int I_IndicatorIndex;
+    public float F_SpreadRadius=5;
+    public float F_StartHeight = 20;
     protected override void OnPlayPreset()
     {
         base.OnPlayPreset();
@@ -14,7 +16,7 @@ public class SFXProjectileCastMeteor : SFXProjectileCastTrigger
     {
         OnPlayPreset();
         float distance = Vector3.Distance(transform.position, targetPosition);
-        Vector3 startPos = targetPosition + Vector3.up * 20 + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)) * distance;
+        Vector3 startPos = targetPosition + Vector3.up * F_StartHeight + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f))*F_SpreadRadius;
         Vector3 spreadDirection = (targetPosition - startPos).normalized;
         m_Simulator = new ProjectilePhysicsSimulator(startPos, spreadDirection, Vector3.down, F_Speed);
         transform.position = startPos;
