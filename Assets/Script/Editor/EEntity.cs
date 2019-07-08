7a873e2548e626f0f2a8be22cab2aa861b08e4b1
StriteR <letsbattle@foxmail.com>
2019-07-08 19:24:09 +0800

    Update EEntity.cs

--- Assets/Script/Editor/EEntity.cs
+++ Assets/Script/Editor/EEntity.cs
@@ -6,40 +6,41 @@ using UnityEngine;
 public class EEntity : Editor {
     PreviewRenderUtility m_Preview;
     GameObject m_PreviewObject;
+    Animator m_PreviewAnimator;
     private void OnEnable()
     {
+        EditorApplication.update += Repaint;
         if (HasPreviewGUI())
         {
             m_Preview = new PreviewRenderUtility();
             m_Preview.camera.fieldOfView = 30.0f;
-            m_Preview.camera.allowHDR = false;
-            m_Preview.camera.allowMSAA = false;
-            m_Preview.ambientColor = new Color(.1f, .1f, .1f, 0);
-            m_Preview.lights[0].intensity = 1.4f;
-            m_Preview.lights[0].transform.rotation = Quaternion.Euler(40f, 40f, 0);
-            m_Preview.lights[1].intensity = 1.4f;
-            m_Preview.camera.transform.position = new Vector3(1, 1f, -6);
-
-
+            m_Preview.camera.nearClipPlane = 0.3f;
+            m_Preview.camera.farClipPlane = 1000;
             m_PreviewObject = m_Preview.InstantiatePrefabInScene(((Component)target).gameObject);
             m_PreviewObject.SetActive(true);
             m_PreviewObject.transform.position = Vector3.zero;
+            m_PreviewAnimator = m_PreviewObject.GetComponentInChildren<Animator>();
+            m_PreviewAnimator.updateMode = AnimatorUpdateMode.Normal;
         }
     }
     private void OnDisable()
     {
+        EditorApplication.update -= Repaint;
         if (HasPreviewGUI())
         {
             m_Preview.Cleanup();
             m_Preview = null;
             m_PreviewObject = null;
+            m_PreviewAnimator = null;
         }
     }
     public override void OnPreviewGUI(Rect r, GUIStyle background)
     {
         m_Preview.BeginStaticPreview(r);
-        m_Preview.camera.transform.position = -(Vector3.forward * 8f);
-        m_Preview.camera.transform.rotation = Quaternion.identity;
+        m_Preview.camera.transform.position = new Vector3(0, 8, -8);
+        m_Preview.camera.transform.LookAt(m_PreviewObject.transform);
+        m_PreviewObject.transform.Rotate(0,1f,0);
+        m_PreviewAnimator.Update(Time.deltaTime);
         m_Preview.camera.Render();
         GUI.DrawTexture(r, m_Preview.EndStaticPreview());
     }
