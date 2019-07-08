fb7ea7166a0388f250a877abd54489f4e1ab8b10
striter <letsbattle@foxmail.com>
2019-07-08 09:38:21 +0800

    Update SFXProjectileCastMeteor.cs

--- Assets/Script/InGame/SFXProjectileCastMeteor.cs
+++ Assets/Script/InGame/SFXProjectileCastMeteor.cs
@@ -15,7 +15,6 @@ public class SFXProjectileCastMeteor : SFXProjectileCastTrigger
     public override void Play(int sourceID, Vector3 direction, Vector3 targetPosition, float duration)
     {
         OnPlayPreset();
-        float distance = Vector3.Distance(transform.position, targetPosition);
         Vector3 startPos = targetPosition + Vector3.up * F_StartHeight + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f))*F_SpreadRadius;
         Vector3 spreadDirection = (targetPosition - startPos).normalized;
         m_Simulator = new ProjectilePhysicsSimulator(startPos, spreadDirection, Vector3.down, F_Speed);
