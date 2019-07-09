00ceccaaade133bd78f8339e41668d3a359a7c6d
striter <letsbattle@foxmail.com>
2019-07-09 10:30:10 +0800

    Animator Preview Update

--- Assets/Script/Editor/EEntity.cs
+++ Assets/Script/Editor/EEntity.cs
@@ -7,9 +7,17 @@ public class EEntity : Editor {
     PreviewRenderUtility m_Preview;
     GameObject m_PreviewObject;
     Animator m_PreviewAnimator;
+    EntityEnermyBase m_EnermyBase;
+    int hs_activate = Animator.StringToHash("t_activate");
+    int hs_attack = Animator.StringToHash("t_attack");
+    int hs_b_attack = Animator.StringToHash("b_attack");
+    int hs_weaponType = Animator.StringToHash("i_weaponType");
+    int hs_forward = Animator.StringToHash("f_forward");
+    Vector3 v3_center;
     private void OnEnable()
     {
-        EditorApplication.update += Repaint;
+        m_EnermyBase = target as EntityEnermyBase;
+        EditorApplication.update += Update;
         if (HasPreviewGUI())
         {
             m_Preview = new PreviewRenderUtility();
@@ -21,11 +29,15 @@ public class EEntity : Editor {
             m_PreviewObject.transform.position = Vector3.zero;
             m_PreviewAnimator = m_PreviewObject.GetComponentInChildren<Animator>();
             m_PreviewAnimator.updateMode = AnimatorUpdateMode.Normal;
+            m_PreviewAnimator.fireEvents = false;
+            v3_center = m_PreviewObject.GetComponentInChildren<MeshRenderer>().bounds.center;
+            m_Preview.camera.transform.position = v3_center + new Vector3(0,8,6);
+            m_Preview.camera.transform.LookAt(m_PreviewObject.transform);
         }
     }
     private void OnDisable()
     {
-        EditorApplication.update -= Repaint;
+        EditorApplication.update -= Update;
         if (HasPreviewGUI())
         {
             m_Preview.Cleanup();
@@ -34,19 +46,40 @@ public class EEntity : Editor {
             m_PreviewAnimator = null;
         }
     }
+    Vector2 rotateDelta;
+    void Update()
+    {
+        Repaint();
+        m_PreviewAnimator.Update(Time.deltaTime);
+    }
     public override void OnPreviewGUI(Rect r, GUIStyle background)
     {
+        if (Event.current != null && Event.current.type == EventType.MouseDrag)
+            m_PreviewObject.transform.Rotate(new Vector3(0,Event.current.delta.x,0) );
         m_Preview.BeginStaticPreview(r);
-        m_Preview.camera.transform.position = new Vector3(0, 8, -8);
-        m_Preview.camera.transform.LookAt(m_PreviewObject.transform);
-        m_PreviewObject.transform.Rotate(0,1f,0);
-        m_PreviewAnimator.Update(Time.deltaTime);
         m_Preview.camera.Render();
+        if (m_PreviewAnimator.GetInteger(hs_weaponType) != (int)m_EnermyBase.E_AnimatorIndex)
+        {
+            m_PreviewAnimator.SetInteger(hs_weaponType,(int)m_EnermyBase.E_AnimatorIndex);
+            m_PreviewAnimator.SetTrigger(hs_activate);
+        }
         GUI.DrawTexture(r, m_Preview.EndStaticPreview());
     }
     public override GUIContent GetPreviewTitle()
     {
         return new GUIContent("Entity Animation Preview");
+    }
+    public override void OnPreviewSettings()
+    {
+        if (GUILayout.Button("Attack"))
+        {
+            m_PreviewAnimator.SetTrigger(hs_attack);
+            m_PreviewAnimator.SetBool(hs_b_attack, !m_PreviewAnimator.GetBool(hs_b_attack));
+        }
+        if (GUILayout.Button("Walk"))
+        {
+            m_PreviewAnimator.SetFloat(hs_forward, m_PreviewAnimator.GetFloat(hs_forward) == 1 ? 0 : 1);
+        }
     }
     public override bool HasPreviewGUI()
     {
