e77562881381cb1d7d62d38015646862b60b0783
s3426887 <31834986+fuzhuo1@users.noreply.github.com>
2019-07-11 10:41:42 +0800

    火焰特效

---
+++ Assets/Fire FX/Demo/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public void CollectionFlamethrower()  {
+		SceneManager.LoadScene ("CollectionFlamethrower");
+	}
+    public void CollectionFull()  {
+        SceneManager.LoadScene ("CollectionFull");
+	}
+    public void CollectionMissiles()  {
+        SceneManager.LoadScene ("CollectionMissiles");
+	}
+    public void CollectionSmall()  {
+		SceneManager.LoadScene ("CollectionSmall");
+	}
+    public void FX1Fire()  {
+        SceneManager.LoadScene ("FX1Fire");
+	}
+    public void FX1FireFull()  {
+        SceneManager.LoadScene ("FX1FireFull");
+	}
+    public void FX1Flamethrower()  {
+        SceneManager.LoadScene ("FX1Flamethrower");
+	}
+    public void FX1Missiles()  {
+        SceneManager.LoadScene ("FX1Missiles");
+    }
+    public void FX2Fire()  {
+        SceneManager.LoadScene ("FX2Fire");
+    }
+    public void FX2FireFull() {
+        SceneManager.LoadScene("FX2FireFull");
+    }
+    public void FX2Flamethrower() {
+        SceneManager.LoadScene("FX2Flamethrower");
+    }
+    public void FX2Missiles() {
+        SceneManager.LoadScene("FX2Missiles");
+    }
+}
---
+++ Assets/Fire FX/Fire FX - Readme.txt
@@ -0,0 +1,19 @@
+------------------
+Stylized Fire FX
+------------------
+
+To use the effects, simply import the Fire FX folder to your project.
+
+Note: The "Demo" folder in the project is not required for the effects to work. This simply contains the web demo and preview scenes.
+
+------------------
+Credits:
+------------------
+
+This Unity asset was created by Kenneth Foldal Moe.
+
+Thanks to Julius Lyngby Forsberg for help with demo programming.
+
+------------------
+
+Feedback and questions can be sent to: archanor.work@gmail.com
