61251f9bf7d59a13c767a5e212b9f78698b87b04
striter <letsbattle@foxmail.com>
2019-07-24 15:54:28 +0800

    Update

--- Assets/Script/InGame/HitCheckBase.cs
+++ Assets/Script/InGame/HitCheckBase.cs
@@ -5,7 +5,7 @@ using System;
 [RequireComponent(typeof(Collider))]
 public class HitCheckBase : MonoBehaviour {
     public virtual enum_HitCheck m_HitCheckType => enum_HitCheck.Invalid;
-    public int I_AttacherID = -1;
+    public int I_AttacherID { get; private set; } = -1;
     Func<DamageInfo, bool> OnHitCheck;
     protected Collider m_Collider;
     protected void Awake()
