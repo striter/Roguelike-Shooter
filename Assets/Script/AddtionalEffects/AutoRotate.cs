767f4e43d3cccd1b8c561921dec59b13d8d7cea8
striter <letsbattle@foxmail.com>
2019-08-06 17:26:05 +0800

    Update

--- Assets/Script/AddtionalEffects/AutoRotate.cs
+++ Assets/Script/AddtionalEffects/AutoRotate.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoRotate : MonoBehaviour {
+    public Vector3 V3_RotateDireciton=Vector3.up;
     public float F_RotateSpeed;
 	void Update () {
-        transform.Rotate(0, F_RotateSpeed * Time.deltaTime, 0);
+        transform.Rotate(V3_RotateDireciton*F_RotateSpeed);
 	}
 }
