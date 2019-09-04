45eaf7a03578ae4a023c826d490e30d0d8c982f7
striter <letsbattle@foxmail.com>
2019-09-04 17:15:04 +0800

    Update

--- Assets/InteractPickupArmor.cs
+++ Assets/InteractPickupArmor.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using GameSetting;
 public class InteractPickupArmor : InteractPickup {
-    public override enum_Interaction m_InteractType => enum_Interaction.PickupHealth;
-    public float m_healAmount { get; private set; }
+    public override enum_Interaction m_InteractType => enum_Interaction.PickupArmor;
+    public float m_ArmorAmount { get; private set; }
     public InteractPickupArmor Play(float _armorAmount)
     {
         base.Play();
-        m_healAmount = _armorAmount;
+        m_ArmorAmount = _armorAmount;
         return this;
     }
     protected override void OnInteractSuccessful(EntityPlayerBase _interactTarget)
     {
         base.OnInteractSuccessful(_interactTarget);
-        _interactTarget.m_HitCheck.TryHit(new DamageInfo(-m_healAmount, enum_DamageType.ArmorOnly, DamageDeliverInfo.Default(_interactTarget.I_EntityID)));
+        _interactTarget.m_HitCheck.TryHit(new DamageInfo(-m_ArmorAmount, enum_DamageType.ArmorOnly, DamageDeliverInfo.Default(_interactTarget.I_EntityID)));
     }
 }
