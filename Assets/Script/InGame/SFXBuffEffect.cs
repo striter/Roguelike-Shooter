783415faedc103843fef4a36cc4b5d6d89b75f69
striter <letsbattle@foxmail.com>
2019-10-16 13:48:55 +0800

    Update SFXBuffEffect.cs

--- Assets/Script/InGame/SFXBuffEffect.cs
+++ Assets/Script/InGame/SFXBuffEffect.cs
@@ -6,7 +6,6 @@ public class SFXBuffEffect : SFXParticles {
     protected override bool m_AutoRecycle => f_duration > 0;
     public void Refresh(float refreshDuration)
     {
-        if (refreshDuration == 0) refreshDuration = 1f;
-            SetLifeTime(refreshDuration );
+        SetLifeTime(refreshDuration);
     }
 }
