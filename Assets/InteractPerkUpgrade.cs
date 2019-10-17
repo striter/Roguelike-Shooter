59d1f945b577c9f9c536d159aca280999bf973e6
striter <letsbattle@foxmail.com>
2019-10-17 10:03:27 +0800

    Update

--- Assets/InteractPerkUpgrade.cs
+++ Assets/InteractPerkUpgrade.cs
@@ -8,12 +8,19 @@ public class InteractPerkUpgrade : InteractBase {
     public override enum_Interaction m_InteractType => enum_Interaction.PerkUpgrade;
     public override bool B_InteractOnce => true;
     ActionBase m_invalidPerk;
+    Animation m_Anim;
     Action OnInteract;
+    public override void Init()
+    {
+        base.Init();
+        m_Anim = GetComponent<Animation>();
+    }
     public void Play(Action _OnInteract,ActionBase _invalidPerk)
     {
         base.Play();
         OnInteract = _OnInteract;
         m_invalidPerk = _invalidPerk;
+        m_Anim.Play();
     }
     protected override bool B_CanInteract(EntityCharacterPlayer _interactor) => _interactor.m_WeaponCurrent.m_WeaponAction == null || _interactor.m_WeaponCurrent.m_WeaponAction.B_Upgradable;
     protected override void OnInteractSuccessful(EntityCharacterPlayer _interactTarget)
@@ -22,5 +29,6 @@ public class InteractPerkUpgrade : InteractBase {
         OnInteract();
         _interactTarget.UpgradeWeaponPerk(m_invalidPerk);
         m_invalidPerk = null;
+        m_Anim.Stop();
     }
 }
