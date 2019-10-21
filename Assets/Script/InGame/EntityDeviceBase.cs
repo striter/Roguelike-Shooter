6a0f0868059fb11ee62fbe7026eb2da5aa834092
striter <letsbattle@foxmail.com>
2019-10-21 16:18:48 +0800

    UpdateUpdate

--- Assets/Script/InGame/EntityDeviceBase.cs
+++ Assets/Script/InGame/EntityDeviceBase.cs
@@ -7,17 +7,24 @@ public class EntityDeviceBase : EntityCharacterBase {
     public override enum_EntityController m_Controller => enum_EntityController.Device; 
     EntityDetector m_Detect;
     protected List<EntityCharacterBase> m_DetectLink=new List<EntityCharacterBase>();
+    ParticleSystem[] m_Particles;
     public override void Init(int _poolIndex)
     {
         base.Init(_poolIndex);
         m_Detect = transform.Find("EntityDetector").GetComponent<EntityDetector>();
         m_Detect.Init(OnEntityDetect);
+        m_Particles = GetComponentsInChildren<ParticleSystem>();
     }
-
+    public override void OnActivate(enum_EntityFlag _flag, float startHealth = 0)
+    {
+        base.OnActivate(_flag, startHealth);
+        m_Particles.Traversal((ParticleSystem particle) => { particle.Play(); });
+    }
     protected override void OnDead()
     {
         base.OnDead();
         m_DetectLink.Clear();
+        m_Particles.Traversal((ParticleSystem particle) => { particle.Stop(); });
     }
 
     void OnEntityDetect(HitCheckEntity entity, bool enter)
