63929845ff49000554039f1631bdba3dc43ac2e3
striter <letsbattle@foxmail.com>
2019-10-28 17:43:20 +0800

    Update

--- Assets/Script/UI/UI_ActionAdjustment.cs
+++ Assets/Script/UI/UI_ActionAdjustment.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 
 public class UI_ActionAdjustment : UIPageBase {
 
-    UIT_GridControllerMonoItem<UIGI_ActionSelectItem> m_Grid;
+    UIT_GridControllerMonoItem<UIGI_ActionItemSelect> m_Grid;
     Button btn_remove, btn_upgrade;
     UIT_TextExtend txt_remove, txt_upgrade,txt_coins;
     int m_selectIndex = -1;
     protected override void Init(bool useAnim)
     {
         base.Init(useAnim);
-        m_Grid = new UIT_GridDefaultSingle<UIGI_ActionSelectItem>(tf_Container.Find("ActionGrid"), OnItemSelected, true);
+        m_Grid = new UIT_GridControllerMonoItem<UIGI_ActionItemSelect>(tf_Container.Find("ActionGrid"));
         txt_coins = tf_Container.Find("Coins").GetComponent<UIT_TextExtend>();
         btn_remove = tf_Container.Find("BtnRemove").GetComponent<Button>();
         btn_remove.onClick.AddListener(OnRemoveClick);
@@ -33,7 +33,7 @@ public class UI_ActionAdjustment : UIPageBase {
         txt_coins.text = m_Interact.m_Interactor.m_Coins.ToString();
         m_Grid.ClearGrid();
         for (int i = 0; i < m_Interact.m_Interactor.m_ActionStored.Count; i++)
-            m_Grid.AddItem(i).SetInfo(m_Interact.m_Interactor.m_ActionStored[i]);
+            m_Grid.AddItem(i).SetInfo(m_Interact.m_Interactor.m_ActionStored[i],OnItemSelected,true);
         m_selectIndex = -1;
         OnAdjustmentBtnStatus();
     }
@@ -41,9 +41,10 @@ public class UI_ActionAdjustment : UIPageBase {
     void OnItemSelected(int index)
     {
         if (m_selectIndex != -1)
-            m_Grid.GetItem(m_selectIndex).SetHighLight(false);
+            m_Grid.GetItem(m_selectIndex).SetHighlight(false);
         m_selectIndex = index;
-        m_Grid.GetItem(m_selectIndex).SetHighLight(true);
+        m_Grid.GetItem(m_selectIndex).SetHighlight(true);
+
        OnAdjustmentBtnStatus();
     }
     void OnAdjustmentBtnStatus()
