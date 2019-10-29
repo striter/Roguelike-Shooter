ba7823d3f2eb280e9555fe7eb92dd560bc11df45
striter <letsbattle@foxmail.com>
2019-10-29 16:34:06 +0800

    Update

--- Assets/Scenes/Test/ShaderTest/ShaderTest.cs
+++ Assets/Scenes/Test/ShaderTest/ShaderTest.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 
 public class ShaderTest : MonoBehaviour
 {
+    public GameObject target;
     void Start ()
     {
         GetComponent<CameraEffectManager>().GetOrAddCameraEffect<CB_GenerateOpaqueTexture>();
+PE_FocalDepth focal= GetComponent<CameraEffectManager>().GetOrAddCameraEffect<PE_FocalDepth>();
+focal.SetEffect(4);
+focal.SetFocalTarget(target.transform.position,1f);
     }
 
 }
