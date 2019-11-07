01a1d36de1075cc448294bd23a0d3ce0293d2c41
striter <letsbattle@foxmail.com>
2019-11-07 15:18:03 +0800

    Update EntityNPC.cs

--- Assets/Script/Game/EntityNPC.cs
+++ Assets/Script/Game/EntityNPC.cs
@@ -3,4 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 using GameSetting;
 public class EntityNPC : EntityBase {
+    public override enum_EntityController m_Controller => enum_EntityController.None;
 }
