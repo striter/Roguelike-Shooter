0d7bc9cb1655ac6165b2057a74c3fa36e2556f40
striter <letsbattle@foxmail.com>
2019-11-26 13:48:19 +0800

    Update UIM_Intro.cs

--- Assets/Script/UI/UIM_Intro.cs
+++ Assets/Script/UI/UIM_Intro.cs
@@ -6,13 +6,11 @@ using UnityEngine;
 public class UIM_Intro : UIMessageBoxBase{
 
     UIT_TextExtend m_title, m_Intro;
-    UIT_TextExtend txt_Confirm;
     protected override void Init()
     {
         base.Init();
         m_title = tf_Container.Find("Title").GetComponent<UIT_TextExtend>();
         m_Intro = tf_Container.Find("Intro").GetComponent<UIT_TextExtend>();
-        txt_Confirm = tf_Container.Find("Confirm/Text").GetComponent<UIT_TextExtend>();
     }
     public void Play(string titleKey, string introKey, Action _OnConfirmClick)
     {
