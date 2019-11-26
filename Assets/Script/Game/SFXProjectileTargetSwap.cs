1dee62704838fdadccd319203c580202f6ceab30
striter <letsbattle@foxmail.com>
2019-11-26 09:43:23 +0800

    Update

--- Assets/Script/Game/SFXProjectileTargetSwap.cs
+++ Assets/Script/Game/SFXProjectileTargetSwap.cs
@@ -10,7 +10,7 @@ public class SFXProjectileTargetSwap : SFXProjectile {
     protected override bool B_StopPhysicsOnHit => false;
     protected override bool B_StopParticlesOnHit => false;
     protected override PhysicsSimulator<HitCheckBase> GetSimulator(Vector3 direction, Vector3 targetPosition) =>new ReflectBouncePSimulator<HitCheckBase>(transform, transform.position, direction, Vector3.down, F_Speed, F_Height, F_Radius,I_BounceCount, OnSwapTarget, GameLayer.Mask.I_All, OnHitTargetBreak, CanHitTarget);
-    protected override float F_PlayDuration(Vector3 startPos, Vector3 endPos) => 10f;
+    protected override float F_PlayDuration(Vector3 startPos, Vector3 endPos) => 5f;
     enum_EntityFlag m_SourceFlag;
     public override void Play(DamageDeliverInfo deliverInfo, Vector3 direction, Vector3 targetPosition)
     {
@@ -20,6 +20,8 @@ public class SFXProjectileTargetSwap : SFXProjectile {
 
     Vector3 OnSwapTarget(Vector3 inforward,Vector3 normal)
     {
+        m_EntityHitted.Clear();
+        SetLifeTime(5f);
         Vector3 direction = Vector3.Reflect(inforward,normal);
         direction.y = 0;
         return direction.normalized;
