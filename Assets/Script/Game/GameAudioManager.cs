7d803103e82141b58326d9da704451ed1a75d268
striter <letsbattle@foxmail.com>
2019-11-29 16:32:26 +0800

    Update GameAudioManager.cs

--- Assets/Script/Game/GameAudioManager.cs
+++ Assets/Script/Game/GameAudioManager.cs
@@ -22,7 +22,6 @@ public class GameAudioManager : AudioManager
             if (clip) m_GameMusic.Add(music, clip);
         });
         TBroadCaster<enum_BC_GameStatus>.Add(enum_BC_GameStatus.OnStageBeginLoad, OnStageBeginLoad);
-        TBroadCaster<enum_BC_GameStatus>.Add<SBigmapLevelInfo>(enum_BC_GameStatus.OnChangeLevel, OnChangeLevel);
         TBroadCaster<enum_BC_GameStatus>.Add(enum_BC_GameStatus.OnBattleStart, OnBattleStart);
         TBroadCaster<enum_BC_GameStatus>.Add(enum_BC_GameStatus.OnBattleFinish, OnBattleFinish); ;
         TBroadCaster<enum_BC_GameStatus>.Add<bool>(enum_BC_GameStatus.OnGameFinish, OnGameFinish);
@@ -31,7 +30,6 @@ public class GameAudioManager : AudioManager
     {
         base.Recycle();
         TBroadCaster<enum_BC_GameStatus>.Remove(enum_BC_GameStatus.OnStageBeginLoad, OnStageBeginLoad);
-        TBroadCaster<enum_BC_GameStatus>.Remove<SBigmapLevelInfo>(enum_BC_GameStatus.OnChangeLevel, OnChangeLevel);
         TBroadCaster<enum_BC_GameStatus>.Remove(enum_BC_GameStatus.OnBattleStart, OnBattleStart);
         TBroadCaster<enum_BC_GameStatus>.Remove(enum_BC_GameStatus.OnBattleFinish, OnBattleFinish); ;
         TBroadCaster<enum_BC_GameStatus>.Remove<bool>(enum_BC_GameStatus.OnGameFinish, OnGameFinish);
@@ -52,33 +50,18 @@ public class GameAudioManager : AudioManager
             m_GameMusic.Add(music, clip);
         });
     }
-
-    void OnChangeLevel(SBigmapLevelInfo info)
-    {
-        switch (info.m_LevelType)
-        {
-            case enum_TileType.Start:
-            case enum_TileType.CoinsTrade:
-            case enum_TileType.ActionAdjustment:
-                PlayBGM(enum_GameMusic.Relax, true);
-                break;
-        }
-    }
-
+    
     void OnBattleStart()
     {
-        switch (GameManager.Instance.m_GameLevel.m_LevelType)
-        {
-            case enum_TileType.ActionAdjustment:
-            case enum_TileType.Battle:
-                PlayBGM(enum_GameMusic.FightRelax, true);
-                break;
-            case enum_TileType.End:
-                PlayBGM(enum_GameMusic.FightHard, true);
-                break;
-        }
+        PlayBGM(GameManager.Instance.m_GameLevel.m_LevelType == enum_TileType.End ? enum_GameMusic.FightHard : enum_GameMusic.FightRelax, true);
     }
-    void OnBattleFinish() =>  Stop();
+    void OnBattleFinish()
+    {
+        if (GameManager.Instance.m_GameLevel.B_IsFinalLevel)
+            Stop();
+        else
+            PlayBGM(enum_GameMusic.Relax,true);
+    }
     void OnGameFinish(bool win) => SetBGPitch(.8f);
 
     void PlayBGM(enum_GameMusic music, bool loop)
