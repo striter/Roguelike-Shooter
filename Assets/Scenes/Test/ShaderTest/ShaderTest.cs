088fe7b22a0578f47b86c5f6546693c5cb814d9d
striter <letsbattle@foxmail.com>
2019-12-10 11:02:20 +0800

    Update

--- Assets/Scenes/Test/ShaderTest/ShaderTest.cs
+++ Assets/Scenes/Test/ShaderTest/ShaderTest.cs
@@ -8,7 +8,7 @@ public class ShaderTest : MonoBehaviour
     public GameObject target;
     void Start ()
     {
-        GetComponent<CameraEffectManager>().GetOrAddCameraEffect<PE_DepthSSAO>().SetEffect();
+        GetComponent<CameraEffectManager>().GetOrAddCameraEffect<PE_Blurs>().SetEffect( PE_Blurs.enum_BlurType.AverageBlur,2,5,2);
         //        GetComponent<CameraEffectManager>().GetOrAddCameraEffect<CB_GenerateOpaqueTexture>();
         //PE_FocalDepth focal= GetComponent<CameraEffectManager>().GetOrAddCameraEffect<PE_FocalDepth>();
         //focal.SetEffect(4);
