5f496626e1cbd44af4cd91458bbe626a6a33ce52
striter <letsbattle@foxmail.com>
2019-12-16 11:19:55 +0800

    Update

--- Assets/Scripts LongHaul/UITools/UIT_EventTriggerListener.cs
+++ Assets/Scripts LongHaul/UITools/UIT_EventTriggerListener.cs
@@ -3,20 +3,28 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UIT_EventTriggerListener : EventTrigger {
-    public Action<bool, Vector2> D_OnPress;
+    public Action<float> OnPressDuration;
+    public Action<bool, Vector2> OnPressStatus;
     public Action<bool,Vector2> D_OnDragStatus;
     public Action<Vector2> D_OnDrag,D_OnDragDelta;
     public Action D_OnRaycast;
+    float m_pressDuration;
     public override void OnPointerDown(PointerEventData eventData)
     {
         base.OnPointerDown(eventData);
-        D_OnPress?.Invoke(true, eventData.position);
+        m_pressDuration = 0f;
     }
     public override void OnPointerUp(PointerEventData eventData)
     {
         base.OnPointerDown(eventData);
-        D_OnPress?.Invoke(false, eventData.position);
+        OnPressDuration?.Invoke(m_pressDuration);
+        m_pressDuration = 0f;
     }
+    private void Update()
+    {
+        m_pressDuration += Time.deltaTime;
+    }
+
     public override void OnDrag(PointerEventData eventData)
     {
         base.OnDrag(eventData);
