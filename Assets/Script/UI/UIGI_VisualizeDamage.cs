e8cdef817407a32dc8d6e1603099a8a36931ec44
striter <letsbattle@foxmail.com>
2020-04-03 17:24:09 +0800

    Update

--- Assets/Script/UI/UIGI_VisualizeDamage.cs
+++ Assets/Script/UI/UIGI_VisualizeDamage.cs
@@ -3,7 +3,7 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 public class UIGI_VisualizeDamage : UIT_GridItem {
-    Text m_Amount,m_Projection;
+    Text m_Amount,m_Projection,m_Critical;
     Action<int> OnAnimFinished;
     RectTransform rtf_SubContainer;
     EntityCharacterBase m_Entity;
@@ -17,20 +17,22 @@ public class UIGI_VisualizeDamage : UIT_GridItem {
         rtf_SubContainer = rtf_Container.Find("SubContainer").GetComponent<RectTransform>();
         m_Amount = rtf_SubContainer.Find("Amount").GetComponent<Text>();
         m_Projection = rtf_SubContainer.Find("Projection").GetComponent<Text>();
+        m_Critical = m_Amount.transform.Find("Critical").GetComponent<Text>();
         rtf_RectTransform.anchoredPosition = UIConst.V2_UINumericVisualizeOffset;
     }
 
-    public void Play(EntityCharacterBase damageEntity,bool criticalHit,float applyAmount,Action<int> _OnAnimFinished)
+    public void Play(EntityCharacterBase _damageEntity,bool _criticalHit,float _applyAmount,Action<int> _OnAnimFinished)
     {
         rtf_SubContainer.anchoredPosition = UIExpression.GetUIDamagePositionOffset();
-        rtf_RectTransform.SetWorldViewPortAnchor(damageEntity.tf_Head.position, CameraController.MainCamera);
-        rtf_SubContainer.localScale = Vector3.one * UIExpression.GetUIDamageScale(applyAmount);
-        m_Animator.SetBool(m_AnimatorIDCritical, criticalHit);
+        rtf_RectTransform.SetWorldViewPortAnchor(_damageEntity.tf_Head.position, CameraController.MainCamera);
+        rtf_SubContainer.localScale = Vector3.one * UIExpression.GetUIDamageScale(_applyAmount);
+        m_Animator.SetBool(m_AnimatorIDCritical, _criticalHit);
+        m_Critical.SetActivate(_criticalHit);
         m_Animator.SetTrigger(m_AnimatorIDPlay);
-        m_Entity = damageEntity;
-        string integer = Mathf.CeilToInt(applyAmount).ToString();
+        m_Entity = _damageEntity;
+        string integer = Mathf.CeilToInt(_applyAmount).ToString();
         m_Amount.text = integer;
-        m_Amount.color = UIExpression.GetUIDamageColor(criticalHit);
+        m_Amount.color = UIExpression.GetUIDamageColor(_criticalHit);
         m_Projection.text = integer;
         OnAnimFinished = _OnAnimFinished;
     }
