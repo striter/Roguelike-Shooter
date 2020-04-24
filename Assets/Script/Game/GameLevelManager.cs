892780b7e4d90e22261ecbb6f36182b4846f4f8e
富卓 <fuzhuo1@qq.com>
2020-04-25 01:20:29 +0800

    update

--- Assets/Script/Game/GameLevelManager.cs
+++ Assets/Script/Game/GameLevelManager.cs
@@ -241,7 +241,7 @@ public class GameLevelManager : SingletonMono<GameLevelManager>, ICoroutineHelpe
                 ConnectGameData(gameChunkGenerate[0], mainConnectionChunks);
 
                 bool subGenerateSuccessful = false;
-                for(int i=0;i<1;i++)
+                for(int i=0;i<2;i++)
                 {
                     bool generateSuccessful = false;
                     mainConnectionChunks.TraversalRandomBreak((ChunkGenerateData mainChunk) =>
