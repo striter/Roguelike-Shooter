86b317d8818fcd461376d47dc62a9a911e78ebd0
ZEE211 <50348908+ZEE211@users.noreply.github.com>
2020-04-27 09:44:24 +0800

    Update GameSetting.cs

--- Assets/Script/Game/GameSetting.cs
+++ Assets/Script/Game/GameSetting.cs
@@ -14,7 +14,7 @@ namespace GameSetting
         public const float F_EnermyGenerateTickMultiplierTransmiting = 2f;
 
         public const float F_EnermyEliteGenerateBase = 10f;
-        public const float F_EnermyEliteGeneratePerMinuteMultiplier = 0.5f;
+        public const float F_EnermyEliteGeneratePerMinuteMultiplier = 2f;
         public const float F_SqrEnermyGenerateMinDistance = 400f; // 20*20
 
         public const float F_SignalTowerTransmitDuration = 40f;
