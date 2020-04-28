6170d766b475e82287f5f91413e54d698b401ea8
striter <letsbattle@foxmail.com>
2020-04-28 10:52:57 +0800

    Update UIC_GameMinimap.cs

--- Assets/Script/UI/UIC_GameMinimap.cs
+++ Assets/Script/UI/UIC_GameMinimap.cs
@@ -86,7 +86,7 @@ public class UIC_GameMinimap : UIControlBase {
     {
         m_Map.DoMapInit();
         m_Stage.text = string.Format("<color=#ffffff88>{0}</color>:<color=#fe9e00>{1}</color>", TLocalization.GetKeyLocalized("UI_MAP_STAGE"),TLocalization.GetKeyLocalized(GameManager.Instance.m_GameLevel.m_GameStage.GetLocalizeKey()));
-        m_Style.text = string.Format("<color=#ffffff88>{0}</color>:<color=#fe9e00>{1}</color>", TLocalization.GetKeyLocalized("UI_MAP_STYLE"), TLocalization.GetKeyLocalized(GameManager.Instance.m_GameLevel.m_GameStage.GetLocalizeKey()));
+        m_Style.text = string.Format("<color=#ffffff88>{0}</color>:<color=#fe9e00>{1}</color>", TLocalization.GetKeyLocalized("UI_MAP_STYLE"), TLocalization.GetKeyLocalized(GameManager.Instance.m_GameLevel.m_GameStyle.GetLocalizeKey()));
     }
 
     private void Update()
