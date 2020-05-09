7ef7ac7bfbbe0b1c1370080c8131b1d48a2bcf31
striter <letsbattle@foxmail.com>
2020-05-09 18:01:03 +0800

    Update

--- Assets/LongHauls/Shaders/UI/UI_OverlayOpaqueBlurBG.shader
+++ Assets/LongHauls/Shaders/UI/UI_OverlayOpaqueBlurBG.shader
@@ -78,5 +78,4 @@
 	ENDCG
 	}
 	}
-	FallBack "Diffuse"
 }
---
+++ Assets/Scenes/TestScenes/ShaderTest/ImageTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<UIT_ImageExtend>().sprite = TResources.GetUIAtlas_Weapon()["detail_101"];
+	}
+	
+}
