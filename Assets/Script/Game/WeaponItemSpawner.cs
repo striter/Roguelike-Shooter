f6ddc04fb5607eb9edefd02e913457ed17822832
striter <letsbattle@foxmail.com>
2020-05-15 13:38:50 +0800

    Update

--- Assets/Script/Game/WeaponItemSpawner.cs
+++ Assets/Script/Game/WeaponItemSpawner.cs
@@ -7,6 +7,6 @@ public class WeaponItemSpawner : WeaponItemBase {
     protected override void OnKeyAnim()
     {
         base.OnKeyAnim();
-        GameObjectManager.SpawnGameCharcter(m_SpawnIndex,NavigationManager.NavMeshPosition( transform.position+TCommon.RandomXZCircle()*2f),Quaternion.identity).OnSubActivate(m_Attacher.m_Flag,m_Attacher.m_EntityID);
+        GameObjectManager.SpawnGameCharcter(m_SpawnIndex,NavigationManager.NavMeshPosition( m_Attacher.transform.position+TCommon.RandomXZCircle()*2f),Quaternion.identity).OnSubActivate(m_Attacher.m_Flag,m_Attacher.m_EntityID);
     }
 }
