926f3139f9c6762002ab541e65ece6a20c230f53
striter <letsbattle@foxmail.com>
2020-05-19 14:36:12 +0800

    Update InteractSignalTower.cs

--- Assets/Script/Game/InteractSignalTower.cs
+++ Assets/Script/Game/InteractSignalTower.cs
@@ -12,6 +12,7 @@ public class InteractSignalTower : InteractGameBase {
     public Transform m_PortalPos { get; private set; }
     Action OnSignalTowerTrigger;
 
+    bool m_Available = false;
     PE_DepthCircleArea m_TransmitArea;
 
     public override void OnPoolInit(enum_Interaction identity, Action<enum_Interaction, MonoBehaviour> OnRecycle)
@@ -26,6 +27,7 @@ public class InteractSignalTower : InteractGameBase {
     {
         base.Play();
         m_Canvas.SetActivate(false);
+        m_Available = false;
         this.OnSignalTowerTrigger = OnSignalTowerTrigger;
         return this;
     }
@@ -39,17 +41,28 @@ public class InteractSignalTower : InteractGameBase {
         return false;
     }
 
+
     public void OnTransmitSet(bool begin)
     {
         m_TransmitArea=CameraController.Instance.m_Effect.SetDepthAreaCircle(begin, transform.position, 10f, .3f, 1f).SetTexture(TResources.m_HolographTex, .5f, new Vector2(.5f, .5f));
         if (!begin)
             m_TransmitArea = null;
+        SetAvailable(true);
     }
 
     public void Tick(float deltaTime,float progress,bool available)
     {
         m_Canvas.rotation = CameraController.CameraProjectionOnPlane(m_Canvas.position);
         m_Count.text = ((int)progress).ToString();
+        SetAvailable(available);
+    }
+
+    void SetAvailable(bool available)
+    {
+        if (m_Available == available)
+            return;
+
         m_TransmitArea.SetColor(available ? TCommon.GetHexColor("2DFFF62F") : TCommon.ColorAlpha(Color.red, .7f), TCommon.GetHexColor("02025EFF"));
+        m_Available = available;
     }
 }
